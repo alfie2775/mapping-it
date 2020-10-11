2ee422e84f92e7793d49419035f236c73556345a
alfie2775 <nunetarun2001@gmail.com>
2020-10-11 21:52:11 +0530

    -1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 if($(document.body).height() < $(window).height()) {
     $("footer").addClass("fixed-bottom");
 }
+
+$(function() {
+    $(".navbar-toggler").blur(function() {
+        $("#nav-bar").collapse('hide');
+    });
+});
